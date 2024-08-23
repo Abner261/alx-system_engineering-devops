d8cb05873532fe34c9706dd498263952c21c9d88
Abner261 <santosabner949@gmail.com>
2024-08-23 16:59:24 +0300

    A recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A recursive function that queries the Reddit API and returns
+"""recursive function that queries the Reddit API and returns
  a list containing the titles of all hot articles for a given subreddit. """
 
 import requests
